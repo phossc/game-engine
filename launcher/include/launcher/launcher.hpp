4af8b596feb359d0be5c4d97d607b80ceb09576a
Philip Schleiter <phossc@protonmail.com>
2018-12-09 00:10:29 +0100

    Launcher entry point and window setup

--- launcher/include/launcher/launcher.hpp
+++ launcher/include/launcher/launcher.hpp
@@ -1,12 +1,22 @@
 #ifndef LAUNCHER_LAUNCHER_HPP
 #define LAUNCHER_LAUNCHER_HPP
 
+#include <engine/core/component.hpp>
+#include <engine/subsystems/window_manager.hpp>
+
 namespace launcher {
 
-class Launcher final {
+class Launcher final : public engine::core::Component {
 public:
+    COMPONENT("57d6303e-d280-4c6a-8a32-11ee891b80db");
+    DEPENDENCIES(engine::subsystems::Window_manager::s_uuid());
+
     void register_components();
     void attach_system_components();
+
+    //! Functions as the launcher's entry point.
+    void activate() override;
+    void deactivate() override;
 };
 
 } // namespace launcher
--- launcher/src/launcher.cpp
+++ launcher/src/launcher.cpp
@@ -1,8 +1,24 @@
 #include <launcher/launcher.hpp>
 
+#include <engine/core/system.hpp>
+
 namespace launcher {
 
-void Launcher::register_components() {}
-void Launcher::attach_system_components() {}
+using engine::core::sys;
+using engine::subsystems::Window_manager;
+
+void Launcher::register_components() {
+    sys->component_registry().register_component<Launcher>();
+}
+
+void Launcher::attach_system_components() {
+    sys->entity().create_component<Launcher>();
+}
+
+void Launcher::activate() {
+    sys->entity().get<Window_manager>()->create_window(1280, 720);
+}
+
+void Launcher::deactivate() {}
 
 } // namespace launcher
