4cd05c1105048a6711559dea089e188e09f567c9
poetastrophe <christoffer@irvall.com>
2019-01-13 16:28:28 +0100

    Added bare input system

---
+++ engine/include/engine/input_system/input_system.hpp
@@ -0,0 +1,16 @@
+#ifndef ENGINE_INPUT_SYSTEM_INPUT_SYSTEM_HPP
+#define ENGINE_INPUT_SYSTEM_INPUT_SYSTEM_HPP
+
+#include <engine/core/component.hpp>
+
+namespace engine {
+
+class Input_system : public core::Component {
+public:
+    COMPONENT("9cbc73de-f5ac-4d04-a18b-775b15889de2");
+    DEPENDENCIES();
+};
+
+} // namespace engine
+
+#endif /* ENGINE_INPUT_SYSTEM_INPUT_SYSTEM_HPP */
