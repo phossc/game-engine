1a41ac4b359a7624bc196cf0bcc7e8a86badf68d
Philip Schleiter <phossc@protonmail.com>
2019-01-24 17:06:07 +0100

    Generic mesh struct and removal of model class

--- engine/include/engine/render_system/mesh.hpp
+++ engine/include/engine/render_system/mesh.hpp
@@ -1,15 +1,19 @@
 #ifndef ENGINE_RENDER_SYSTEM_MESH_HPP
 #define ENGINE_RENDER_SYSTEM_MESH_HPP
 
-#include "glad/glad.h"
-
 #include <vector>
 
 namespace engine {
 
+template <typename VertexFormat>
 struct Mesh {
-    std::vector<GLfloat> vertex_positions;
-    std::vector<GLuint> indices;
+    std::vector<VertexFormat> vertices;
+};
+
+template <typename VertexFormat, typename IndexType>
+struct Mesh_indexed {
+    std::vector<VertexFormat> vertices;
+    std::vector<IndexType> indices;
 };
 
 } // namespace engine
